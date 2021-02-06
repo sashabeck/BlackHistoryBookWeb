17bd0b2c1cb719a2ecb8d0d8f6b81bb7870aea7c
Katie Li <Katie@Katies-MacBook-Pro.local>
2021-02-06 10:29:06 -0500

    Drop downs created, submits form information, redirects to another page

--- app.py
+++ app.py
@@ -1,6 +1,42 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/', methods=['POST','GET'])
+def home():
+    return render_template("index.html")
+
+@app.route('/result/<genre>/<length>')
+def success(genre, length):
+    return render_template("submit.html", genre=genre, length=length)
+
+@app.route('/submitted',methods = ['POST', 'GET'])
+def submit():
+    if request.method == 'POST':
+        genre = request.form["genreDropdown"]
+        length = request.form["lengthDropdown"]
+        return redirect(url_for("success", genre=genre, length=length))
+
+
+
+if __name__ == "__main__":
+    app.run()
+
+# Everything below is reference -----------------------------
+
+'''
+# enter anything into the url after /, it will repeat itself
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}!"
+
+# calls the user function, passes in an argument for the parameter, <name>
+@app.route('/admin')
+def admin():
+    return redirect(url_for("user", name="Admin!"))
+
+
+# redirects back to the home page
+@app.route("/admin")
+def admin():
+    return redirect(url_for("home"))
+'''
